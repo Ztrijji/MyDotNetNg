d5ff28097cef06ba21a5d38f2df7990fef5820de
Alexander Ingarv <aingarv@gmail.com>
2017-10-24 16:58:50 +0200

    The Hero Editor implemented

--- TourOfHeroes/angular-tour-of-heroes/src/app/app.component.ts
+++ TourOfHeroes/angular-tour-of-heroes/src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `<h1>{{title}}</h1>
+             <h2>{{hero.name}} details!</h2>
+             <div><label>id: </label></div>
+             <div>
+              <label>name: </label>
+              <input [(ngModel)]="hero.name" placeholder="name">
+             </div>`
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+
+  }
+
+}
+export class Hero {
+  id: number;
+  name: string;
 }
