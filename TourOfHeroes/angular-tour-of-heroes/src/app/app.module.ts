dda7696723f0160a33e1bac85523e60d333af037
Alexander Ingarv <aingarv@gmail.com>
2017-10-24 18:50:42 +0200

    split into components

--- TourOfHeroes/angular-tour-of-heroes/src/app/app.module.ts
+++ TourOfHeroes/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
