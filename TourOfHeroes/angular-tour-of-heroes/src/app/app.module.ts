abe86d97fe6d340a1d588d7a0d6e3339fe254fa2
Alexander Ingarv <aingarv@gmail.com>
2017-10-24 19:35:07 +0200

    Added services and remote data simulation

--- TourOfHeroes/angular-tour-of-heroes/src/app/app.module.ts
+++ TourOfHeroes/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroDetailComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- TourOfHeroes/angular-tour-of-heroes/src/app/hero-detail.component.ts
+++ TourOfHeroes/angular-tour-of-heroes/src/app/hero-detail.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
-
 import { Hero } from './hero';
+
 @Component({
   selector: 'hero-detail',
   template: `
     <div *ngIf="hero">
       <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>id: </label>{{hero.id}}
+      </div>
       <div>
         <label>name: </label>
         <input [(ngModel)]="hero.name" placeholder="name"/>
